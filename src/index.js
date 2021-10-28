8795f7875ed39771673004fb7640bd80f1839d48
Tadej <tadej@minimaa.co>
2021-10-28 19:58:04 +0200

    Initial

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export default function Button () {
+  return(
+    <Wrapper>
+      Hello
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  width: 50px;
+  height: 30px;
+  color: white;
+  background: red;
+  border: 1px solid black
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer 
+`
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require("webpack");
+
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ],
+            plugins: ["babel-plugin-styled-components"]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {},
+  plugins: [
+    new webpack.ProvidePlugin({
+      "React": "react",
+    })
+  ],
+  externals: {
+    react: "react"
+  },
+}
