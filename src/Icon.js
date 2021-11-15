ddcff3deede8bf6fd117bd30c7e07ee0066c9453
Tadej <tadej@minimaa.co>
2021-11-15 16:33:45 +0100

    Minor

---
+++ src/Icon.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+
+const Icon = ({ active, icon, margin, width, height, open, onClick, mobileWidth, color }) => {
+
+  return(
+    <Svg color={color} onClick={onClick} open={open} width={width} height={height} margin={margin} active={active}>
+      <use xlinkHref={`../raw/${icon}.svg#${icon}`} />
+    </Svg>
+  )
+}
+
+const Svg = styled.svg(props => ({
+  margin: props.margin,
+  width: props.width ? props.width : 15,
+  minWidth: props.width ? props.width : 15,
+  height: props.height ? props.height : 15,
+  minHeight: props.height ? props.height : 15,
+  transform: props.open ? "rotate(180deg)" : "rotate(0deg)",
+  cursor:"pointer",
+  stroke: props.color ? props.color : "currentColor",
+  "@media (max-width: 650px)": {
+    width: props.mobileWidth ? props.mobileWidth : props.width,
+    minWidth: props.mobileWidth ? props.mobileWidth : props.width,
+    height: props.mobileWidth  ? props.mobileWidth : props.height,
+    minHeight: props.mobileWidth  ? props.mobileWidth : props.height,
+  },
+}));
+
+
+export default Icon
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+export * from "./Icon"
 
 export function Button () {
   return(
